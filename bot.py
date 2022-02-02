075d4c55ff3ce925611883845205d720cabfbc68
Davies Nzan <63256994+xandee-games101@users.noreply.github.com>
2022-02-02 01:59:00 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,21 @@
 import telebot
 from telebot import types
-from telebot.types import LabeledPrice, ShippingOption
+from telebot.types import LabeledPrice
 
-API = "1854723686:AAFTyVNy4TlCJWzQVQ_avT0jA9RDPscFGrg"
-STRIPE = "284685063:TEST:YWYyZWU2ZDc3ZmIw"
+API = "5162650515:AAHSMSjEEwJN9QAryqxuAUhaSUt_-y56Nhg"
+STRIPE = "284685063:TEST:ZGQyMDBkZjY4ZTVk"
 
 provider_token = STRIPE  # @BotFather -> Bot Settings -> Payments
 bot = telebot.TeleBot(API)
-
-prices = [LabeledPrice(label='Цена', amount=10000), LabeledPrice('Комиссия', 0)]
+print("started")
+prices = [LabeledPrice(label='Baby dragon', amount=100000), LabeledPrice('Time machine', 50000)]
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     menu = types.ReplyKeyboardMarkup()
     menu.row('Products')
-    menu.row('About the bot')
+    menu.row('About')
     bot.send_message(message.chat.id, 'Hello , {name} \nSelect a menu item :.'.format(name=message.chat.first_name),
                      reply_markup=menu)
 
@@ -25,20 +25,20 @@ def body(message):
     if message.text == 'Products':
         bot.send_message(message.chat.id,
                          "You will recieve an invoice now."
-                         " Use this card number: `4242 4242 4242 4242`", parse_mode='Markdown')
+                         " Use this card number: <code>4242 4242 4242 4242</code>\n\n<b>Do not use your real card as funds can be lost</b>", parse_mode='HTML')
         bot.send_invoice(
             chat_id=message.chat.id,
-            title='Тест',
+            title='Purchase',
             description='You will make a test transaction.',
             invoice_payload='true',
             provider_token=provider_token,
             start_parameter='true',
-            currency='rub',
+            currency='usd',
             prices=prices
         )
     elif message.text == 'About the bot':
         bot.send_message(message.chat.id,
-                         'This is a bot store with Yandex cashier.\nWant to yourself the same?\nCan you order it from us @LifeCode_Bot')
+                         'This is a bot made by xandee for a showcase on how telegram bots and payments work.\nThe bot is not just limited to only payments as it can do limitless things even to crypto airdrop bots')
 
 
 @bot.pre_checkout_query_handler(func=lambda query: True)
@@ -51,9 +51,8 @@ def checkout(pre_checkout_query):
 @bot.message_handler(content_types=['successful_payment'])
 def got_payment(message):
     bot.send_message(message.chat.id,
-                     'You have successfully created a transaction on `{} {}`! '.format(
-                         message.successful_payment.total_amount / 100, message.successful_payment.currency),
-                     parse_mode='Markdown')
+                     'You have successfully created a transaction of ${}'.format(
+                         message.successful_payment.total_amount / 100))
 
 
 bot.skip_pending = True
